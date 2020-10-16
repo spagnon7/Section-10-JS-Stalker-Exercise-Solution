03947d2a0d7091ff4db07c8ff256a53169507048
spagnon7 <spagnon7@gmail.com>
2020-10-15 17:35:52 -0700

    completed exercise

---
+++ script.js
@@ -0,0 +1,5 @@
+firstName = prompt('What is your first name?');
+lastName = prompt('What is your last name?');
+age = prompt('How old are you?');
+console.log('Your full name is ' + firstName + ' ' + lastName + '.');
+console.log('You are ' + age + ' years old.');
